4567fc74e922c35d2ebb18ad9b4c2e31af52f854
knknkn1162 <knknkn1162@gmail.com>
2024-04-13 19:02:48 +0900

    bugfix bastion.user_data

--- linuxvm_rds/bastion.tf
+++ linuxvm_rds/bastion.tf
@@ -8,6 +8,7 @@ module "bastion" {
   subnet_id = module.subnet4public.id
   vm_spec = var.bastion_spec
   user_data = <<-EOF
+#!/bin/bash
 sudo yum update
 sudo yum -y install postgresql15.x86_64
 EOF
