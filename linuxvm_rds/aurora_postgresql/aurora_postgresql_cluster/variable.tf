16ded264b0d0f793097ee3e1b25019042b4af34b
knknkn1162 <knknkn1162@gmail.com>
2024-04-13 20:03:46 +0900

    rename

---
+++ linuxvm_rds/aurora_postgresql/aurora_postgresql_cluster/variable.tf
@@ -0,0 +1,11 @@
+variable "password" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
--- linuxvm_rds/aurora_postgresql/variable.tf
+++ linuxvm_rds/aurora_postgresql/variable.tf
@@ -1,11 +1,23 @@
-variable "password" {
+variable "vpc_cidr" {
   type = string
+}
+
+variable "bastion_spec" {
+  type = string
+}
+
+variable "public_cidr" {
+  type = string
+}
+
+variable "db_cidrs" {
+  type = list(string)
 }
 
 variable "db_name" {
   type = string
 }
 
-variable "subnet_ids" {
-  type = list(string)
+variable "db_password" {
+  type = string
 }
